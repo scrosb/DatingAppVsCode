021bdb2b5b01ecb086b78ca01cf2da842d80af8f
silas <scrosb@gmail.com>
2021-07-17 19:00:32 -0500

    End of Section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -23,9 +23,6 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe(response => {
       //navigate to members component
       this.router.navigateByUrl('/members');
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
     })
   }
 
